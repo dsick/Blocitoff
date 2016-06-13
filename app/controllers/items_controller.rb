aed9753b8b840cb30844891bdf13edaf0561195c
dsick <sick@davesick.net>
2016-06-13 13:00:19 -0700

    fixed form and added delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,28 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
 
+  
   def create
     @user = User.find(params[:user_id])
-    @item = @user.item.build 
+    #@item = @user.items.build(name: params[:item][:name])
+    @item = @user.items.build( params.require(:item).permit(:name) )
 
     if @item.save
       flash[:notice] = "You added an item"
     else
       flash[:alert] = "Was not able to add item. Please try again."
+    end
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "You deleted an item"
+    else
+      flash[:alert] = "Was not able to delete item. Please try again."
     end
     redirect_to user_path(@user)
   end
